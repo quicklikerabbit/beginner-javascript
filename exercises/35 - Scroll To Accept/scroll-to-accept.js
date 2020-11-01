58629b29fdcf858d22888efe5599b6f9d655674a
Steven Richards <steve@syncline.ca>
2020-11-01 12:37:15 -0800

    scroll to accept

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,16 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    this.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
